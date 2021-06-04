d309d47611db02b11595ca44037b68fa939ef550
IainZhuo <32986734+IainZhuo@users.noreply.github.com>
2021-06-04 10:27:12 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>Hello Vue 3.0 + Vite.</h2>
+  <p>
+    <span>Count is: {{ count }} &nbsp;&nbsp;</span>
+    <button @click="setCount(count + 1)">increment</button>
+  </p>
+</template>
+
+<script>
+import { useState } from 'vue3-hooks';
+
+export default {
+  name: 'App',
+  setup() {
+    const [count, setCount] = useState(0);
+
+    return {
+      count,
+      setCount,
+    };
+  },
+};
+</script>
+
+<style>
+@import './index.scss';
+h2 {
+  color: teal;
+}
+</style>
+
